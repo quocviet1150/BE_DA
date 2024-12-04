6a95e0a750f76aed419bf04f3eb6a6618bbb152a
admin <khoahocmaytinh37@gmail.com>
2024-12-04 23:26:50 +0700

    vietnq commit

--- src/main/java/com/coverstar/dto/AccountCreateDto.java
+++ src/main/java/com/coverstar/dto/AccountCreateDto.java
@@ -1,42 +1,24 @@
 package com.coverstar.dto;
 
-import com.coverstar.validator.account.ValidCreateEmail;
-import com.coverstar.validator.account.ValidCreateUsername;
-import com.coverstar.validator.account.ValidEmail;
-import com.coverstar.validator.account.ValidPassword;
-import com.coverstar.validator.account.ValidRepeatPassword;
-import com.coverstar.validator.account.ValidUsername;
 import lombok.Getter;
 import lombok.Setter;
 
-@ValidRepeatPassword
 @Getter
 @Setter
 public class AccountCreateDto {
 
-	private Long id;
-	
-	@ValidUsername
-	@ValidCreateUsername
-	private String username;
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String repeatPassword;
 
-	@ValidEmail
-	@ValidCreateEmail
-	private String email;
-
-	private String firstName;
-
-	private String lastName;
-
-	@ValidPassword
-	private String password;
-
-	private String repeatPassword;
-
-	@Override
-	public String toString() {
-		return "AccountCreateDto [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
-				+ ", repeatPassword=" + repeatPassword + "]";
-	}
+    @Override
+    public String toString() {
+        return "AccountCreateDto [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+                + ", repeatPassword=" + repeatPassword + "]";
+    }
 
 }
--- src/main/java/com/coverstar/dto/AccountUpdateDto.java
+++ src/main/java/com/coverstar/dto/AccountUpdateDto.java
@@ -1,37 +1,16 @@
 package com.coverstar.dto;
 
-import com.coverstar.validator.account.ValidEmail;
-import com.coverstar.validator.account.ValidPassword;
-import com.coverstar.validator.account.ValidRepeatPassword;
-import com.coverstar.validator.account.ValidUpdateEmail;
-import com.coverstar.validator.account.ValidUpdateUsername;
-import com.coverstar.validator.account.ValidUsername;
 import lombok.Getter;
 import lombok.Setter;
 
-@ValidRepeatPassword
-@ValidUpdateEmail
-@ValidUpdateUsername
 @Getter
 @Setter
 public class AccountUpdateDto {
 
 	private Long id;
-	
-	@ValidUsername
-	private String username;
-	
-	@ValidEmail
-	private String email;
-	
-	@ValidPassword
-	private String password;
-	private String repeatPassword;
-
-	@Override
-	public String toString() {
-		return "AccountUpdateDto [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
-				+ ", repeatPassword=" + repeatPassword + "]";
-	}
-
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private Integer sex;
+	private String phoneNumber;
 }
