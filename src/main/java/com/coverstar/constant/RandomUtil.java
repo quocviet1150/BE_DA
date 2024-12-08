7ce5bb5c25cf91c1f09c242eecc60c4e4d84f60e
admin <khoahocmaytinh37@gmail.com>
2024-12-09 00:09:43 +0700

    vietnq commit

--- src/main/java/com/coverstar/constant/RandomUtil.java
+++ src/main/java/com/coverstar/constant/RandomUtil.java
@@ -42,19 +42,10 @@ public class RandomUtil {
 
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-
-			// 0-62 (exclusive), random returns 0-61
             int rndCharAt = random.nextInt(DATA_FOR_RANDOM_STRING.length());
             char rndChar = DATA_FOR_RANDOM_STRING.charAt(rndCharAt);
-
-            // debug
-            System.out.format("%d\t:\t%c%n", rndCharAt, rndChar);
-
             sb.append(rndChar);
-
         }
-
         return sb.toString();
-
     }
 }
