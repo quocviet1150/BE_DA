9fb5f503d0641500d338d94a36f76d1760746f8c
admin <khoahocmaytinh37@gmail.com>
2024-12-09 23:43:51 +0700

    vietnq commit

--- src/main/java/com/coverstar/constant/Constants.java
+++ src/main/java/com/coverstar/constant/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final String VALID_VERIFICATION = "Valid verification code. Account has been activated.";
     public static final String VERIFYING_ERROR = "An error occurred while verifying the code.";
     public static final String PURCHASE_NOT_FOUND = "Purchase not found";
+    public static final String LANGUAGE_VI = "vi";
+    public static final String LANGUAGE_EN = "en";
 
     public static final class Number {
         public static final String ONE = "1";
@@ -17,6 +19,27 @@ public class Constants {
         public static final String EIGHT = "8";
         public static final String NINE = "9";
         public static final String TEN = "10";
+    }
+
+    public class Parameter {
+        public static final String PARTNER_CODE = "partnerCode";
+        public static final String PARTNER_CLIENT_ID = "partnerClientId";
+        public static final String CALLBACK_TOKEN = "callbackToken";
+        public static final String DESCRIPTION = "description";
+        public static final String ACCESS_KEY = "accessKey";
+        public static final String REQUEST_ID = "requestId";
+        public static final String AMOUNT = "amount";
+        public static final String ORDER_ID = "orderId";
+        public static final String ORDER_INFO = "orderInfo";
+        public static final String REQUEST_TYPE = "requestType";
+        public static final String EXTRA_DATA = "extraData";
+        public static final String MESSAGE = "message";
+        public static final String PAY_URL = "payUrl";
+        public static final String RESULT_CODE = "resultCode";
+        public static final String REDIRECT_URL = "redirectUrl";
+        public static final String IPN_URL = "ipnUrl";
+        public static final String TOKEN = "token";
+        public static final String TRANS_ID = "transId";
     }
 
 }
