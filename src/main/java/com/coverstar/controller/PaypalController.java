2c77608d74eef7bec6a0f80161fc0bf025d47eea
vietnq <>
2024-12-10 16:26:35 +0700

    commit code

--- src/main/java/com/coverstar/controller/PaypalController.java
+++ src/main/java/com/coverstar/controller/PaypalController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequiredArgsConstructor
